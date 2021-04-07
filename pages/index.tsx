a7d6db240da1d1fd7effe4f76a433b45f3356388
Devin Ford <devin.d.ford@gmail.com>
2021-04-07 06:15:46 -0400

    trying to fix the head

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,19 +2,13 @@ import Head from 'next/head';
 import Footer from '../components/Footer';
 import Articles from '../components/Articles';
 import Header from '../components/Header';
-import{ useState } from 'react';
+import { useState } from 'react';
 
 export default function Home() {
   const [sortingOrder, setSortingOrder] = useState<string>('');
 
   return (
     <>
-      <Head>
-        <title>dev.to Rising!</title>
-        <link rel='icon' href='/dev-ecosystem.png' />
-        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
-        <meta charSet='utf-8' />
-      </Head>
       <div className='bg-purple-900 min-h-screen flex flex-col justify-start'>
         <Header setSortingOrder={setSortingOrder} />
         <Articles sortingOrder={sortingOrder} />
